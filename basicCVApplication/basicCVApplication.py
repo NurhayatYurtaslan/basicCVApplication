2b52d9a3acfa8c0c7fe71308054e730564435f6b
nurhayat yurtaslan <nurhayatyurtaslan38@gmail.com>
2021-05-28 04:40:36 +0300

    Add project files.

---
+++ basicCVApplication/basicCVApplication.py
@@ -0,0 +1,22 @@
+
+CV={"CV1":"Nurhayat","CV2":"Aslı","CV3":"Derin","CV4":"Engin","CV5":"Murat"}
+print("CV senders",CV.values())
+print("CV number and CV senders",CV)
+
+CV1={"University":"Erciyes","Job":"Engineer","Foreign Language":"English","Hobby":"Read a book"}
+CV2={"University":"Istanbul Teknik","Job":"Engineer","Foreign Language":"Spanish","Hobby":"Read a book"}
+CV3={"University":"Erciyes","Job":"Engineer","Foreign Language":"English","Hobby":"Listen to music"}
+CV4={"University":"Selcuklu","Job":"Engineer","Foreign Language":"Turkish","Hobby":"play game"}
+CV5={"University":"Erciyes","Job":"Engineer","Foreign Language":"English","Hobby":"Read a book"}
+
+print("CV1",CV1 )
+print("*******************************************************")
+print("CV2",CV2 )
+print("*******************************************************")
+print("CV3",CV3 )
+print("*******************************************************")
+print("CV4",CV4 )
+print("*******************************************************")
+print("CV5",CV5 )
+print("*******************************************************")
+
